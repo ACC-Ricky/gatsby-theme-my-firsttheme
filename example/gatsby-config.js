b17619f7a461e253d40bebd8709f2f4b5b5cd5da
RickyH <rickyhobbs@acc.edu.au>
2020-02-06 09:34:34 +1100

    initial commit - test

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,3 +1,23 @@
-module.exports = {
-  plugins: [{ resolve: `gatsby-theme-minimal`, options: {} }],
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV !== 'production'){
+  dotenv.config()
 }
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {}
+      },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `axj298k780o2`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: '2ZBshtIBtciTPryliHc1FcphssgHg-Fvq6Y3Sz-K_RY',
+        host: `preview.contentful.com`,
+      },
+    },
+  ],
+};
--- gatsby-theme-minimal/gatsby-config.js
+++ gatsby-theme-minimal/gatsby-config.js
@@ -1,1 +1,21 @@
-module.exports = {}
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV !== 'production'){
+    dotenv.config()
+}
+// require("dotenv").config({
+//     path: `.env.${process.env.NODE_ENV}`,
+// })
+module.exports = {
+    plugins: [
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+                spaceId: `axj298k780o2`,
+                // Learn about environment variables: https://gatsby.dev/env-vars
+                accessToken: '2ZBshtIBtciTPryliHc1FcphssgHg-Fvq6Y3Sz-K_RY',
+                host: `preview.contentful.com`,
+            },
+        },
+    ],
+};
